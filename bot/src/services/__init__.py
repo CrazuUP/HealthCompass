222ab083cd466f785935721b39bbb3f41dbecb58
CrazyUP <92596340+CrazuUP@users.noreply.github.com>
2025-11-14 17:59:58 +0300

    Add files via upload

---
+++ bot/src/services/__init__.py
@@ -0,0 +1,13 @@
+from .max_api import MaxApiService
+from .health_service import HealthService
+from .screening_service import ScreeningService
+from .community_service import CommunityService
+from .symptom_checker import SymptomChecker
+
+__all__ = [
+    'MaxApiService',
+    'HealthService',
+    'ScreeningService',
+    'CommunityService',
+    'SymptomChecker'
+]
