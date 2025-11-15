58f868307ce7039a71c9919fc1e81f293590a950
CrazyUP <92596340+CrazuUP@users.noreply.github.com>
2025-11-15 05:58:36 +0300

    Delete src directory

--- src/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .message_handler import MessageHandler
-from .callback_handler import CallbackHandler
-from .webhook_handler import WebhookHandler
-
-__all__ = [
-    "MessageHandler",
-    "CallbackHandler",
-    "WebhookHandler"
-]
